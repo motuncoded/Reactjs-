3af5e0634906eae38950e448ce4910eaac787379
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-02-27 17:45:50 +0100

    calaculator

--- beginner-project/src/App.jsx
+++ beginner-project/src/App.jsx
@@ -3,7 +3,9 @@ import viteLogo from '/vite.svg'
 import reactLogo from "./assets/react.svg";
 import './App.css'
 import Nav from './components/Nav'
-
+import { UserProvider } from './components/UserContext';
+import { ThemeProvider } from './components/ThemeContext';
+import { useTheme } from "../ThemeContext";
 
 
 function HeaderApp() {
@@ -13,6 +15,7 @@ function HeaderApp() {
 }
  
 function App() {
+  const {theme, toggleTheme} = useTheme()
   const asideStyle = {
   backgroundColor:"red",
   color:"#fff"
@@ -20,13 +23,24 @@ function App() {
   {/* const Logo = <img src={ reactLogo} alt="logo" />
 return Logo;*/}
   return (
-    <div style={asideStyle}>
+    <ThemeProvider>
+    <UserProvider>
+        <div style={asideStyle}>
+            <label className="switch">
+     <input
+       type="checkbox"
+       checked={theme === "light"}
+       onChange={toggleTheme}
+     />
+     <span className="slider round" />
+   </label>
       <Nav/>
     <HeaderApp/>
       {/* <Logo />*/}
-      
+            </div>
 
-      </div>
+</UserProvider>
+</ThemeProvider>
     )  
 }
 
