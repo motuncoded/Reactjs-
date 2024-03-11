538b90524414196f874a9427f2bcb9a174b73e99
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-03-11 21:07:30 +0100

    feat: create dependency for the project

---
+++ Sign-up form/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import SignUp from "./components/SignUp";
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+     <SignUp/>
+    </div>
+  )
+}
+
+export default App
